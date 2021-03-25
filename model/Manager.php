987c191789c5ab54e49789d71bf7427e352d5fae
melaniegravelle <melanie@ikigaidev.fr>
2021-03-25 09:29:32 +0100

    chg de db

--- model/Manager.php
+++ model/Manager.php
@@ -5,9 +5,13 @@ use classes\dbConnect;
 
 class Manager
 {
-    private $_dsn           = 'mysql:host=localhost:3306;dbname=blog';
-    private $_login         = 'blog';
-    private $_password      = 'blog';
+    // private $_dsn           = 'mysql:host=localhost:3306;dbname=blog';
+    // private $_login         = 'blog';
+    // private $_password      = 'blog';
+
+    private $_dsn           = 'mysql:host=localhost:3306;dbname=ppe3_group2';
+    private $_login         = 'melanie';
+    private $_password      = 'fiH3uj7x';
 
 
     /**
