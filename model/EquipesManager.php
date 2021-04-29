f1aff25325c595ad529cc0cc4d830624920bf3d5
melaniegravelle <melanie@ikigaidev.fr>
2021-04-29 18:19:29 +0200

    maj

--- model/EquipesManager.php
+++ model/EquipesManager.php
@@ -19,4 +19,22 @@ class EquipesManager extends Manager
         // var_dump($listEquipes);die;
         return $listEquipes;
     }
+
+    public function createEquipe(Equipes $equipes)
+    {
+        $q = $this->manager
+                    ->db
+                    ->prepare('INSERT INTO equipes (nom_equipe, nom_entraineur, logo)
+                                VALUES ( :nom_equipe, :nom_entraineur, :logo)'
+                    );
+        $ret = $q->execute([
+            ':nom_equipe' => $equipes->getNomEquipe(),
+            ':nom_entraineur' => $equipes->getNomEntraineur(),
+            ':logo' => $equipes->getLogo(),     
+        ]);
+        if( $ret ) {
+            $ret = $this->manager->db->lastInsertId();
+        }
+        return $ret;
+    }
 }
